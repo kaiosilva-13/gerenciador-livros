45d705f137abcfc48bc4994a985a274bdff2bca1
kaiosilva-13 <kaiovitor13052005@gmail.com>
2026-06-27 17:23:34 -0300

    criação dos artefatos biblioteca

---
+++ src/biblioteca/biblioteca.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { BibliotecaService } from './biblioteca.service';
+import { BibliotecaController } from './biblioteca.controller';
+
+@Module({
+  providers: [BibliotecaService],
+  controllers: [BibliotecaController],
+})
+export class BibliotecaModule {}
