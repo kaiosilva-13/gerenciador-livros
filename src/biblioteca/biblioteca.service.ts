af3dd9056a900338ffab67dff884c1e2117d0811
kaiosilva-13 <kaiovitor13052005@gmail.com>
2026-07-06 09:14:58 -0300

    criação da estrutura do objeto Livro

--- src/biblioteca/biblioteca.service.ts
+++ src/biblioteca/biblioteca.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
+type Livro = {
+    id: number;
+    titulo: string;
+    autor:string;
+    categoria:string;
+    anoPublicacao?:number;
+    isEmprestado:boolean;
+}
+
 @Injectable()
 export class BibliotecaService {}
