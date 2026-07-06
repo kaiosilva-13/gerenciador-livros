ebaa1d0bd15e26e32fa52e7d9d90e59e4b27cee3
kaiosilva-13 <kaiovitor13052005@gmail.com>
2026-07-06 09:39:51 -0300

    criação do método listar pra uso futuro de querys

--- src/biblioteca/biblioteca.service.ts
+++ src/biblioteca/biblioteca.service.ts
@@ -1,13 +1,35 @@
 import { Injectable } from '@nestjs/common';
 
 type Livro = {
-    id: number;
-    titulo: string;
-    autor:string;
-    categoria:string;
-    anoPublicacao?:number;
-    isEmprestado:boolean;
-}
+  id: number;
+  titulo: string;
+  autor: string;
+  categoria: string;
+  anoPublicacao?: string;
+  isEmprestado: boolean;
+};
 
 @Injectable()
-export class BibliotecaService {}
+export class BibliotecaService {
+  private livros: Livro[] = [];
+
+  listar(categoria?: string, autor?: string, anoPublicacao?: string) {
+    let resultado = [...this.livros];
+
+    if (autor) {
+      resultado = resultado.filter((l) => l.autor === autor);
+    }
+
+    if (categoria) {
+      resultado = resultado.filter((l) => l.categoria === categoria);
+    }
+
+    if (anoPublicacao) {
+      resultado = resultado.filter(
+        (l) => l.anoPublicacao === anoPublicacao,
+      );
+    }
+
+    return resultado;
+  }
+}
