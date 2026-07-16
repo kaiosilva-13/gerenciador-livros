228424af827f6cb08c41ba8151a1ebde486a50e6
kaiosilva-13 <kaiovitor13052005@gmail.com>
2026-07-15 21:10:06 -0300

    Criação , configuração e integração do controller do artefato de usuarios

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { UsuariosService } from './usuarios.service';
 @Controller('usuarios')
-export class UsuariosController {}
+export class UsuariosController {
+  constructor(private readonly usuariosService: UsuariosService) {}
+  @Get()
+  listarTodos() {
+    return this.usuariosService.listarTodos();
+  }
+  @Get(':id')
+  buscarPorId(@Param('id', ParseIntPipe) id: number) {
+    return this.usuariosService.buscarPorId(id);
+  }
+}
